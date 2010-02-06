c9638b58ccb566d73fa03b35a433eb11504c6267
Randy Fischer <rf@ufl.edu>
2010-02-05 23:45:14 -0500

    capistrano rules

---
+++ Capfile
@@ -0,0 +1,4 @@
+load    'deploy' if respond_to?(:namespace)  # cap2?
+require 'rubygems'
+require 'railsless-deploy'
+load    'config/deploy'
---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+ set :application,       "xmlresolution"
+ set :domain,            "xmlresolution.dev.fcla.edu"
+ set :repository,        "ssh://retsina.fcla.edu//var/git/#{application}.git"
+ set :local_repository,  "file:///var/git/#{application}.git"
+ set :use_sudo,          false
+ set :deploy_to,         "/home/fischer/test-deploy/#{application}"
+ set :scm,               "git"
+#set :gateway,		 "gnvssh.fcla.edu:2020"   # what *is* my password?
+#set :gateway,		 "fcla"                   # a host/port alias from .ssh/config
+
+ role :app, domain
+ role :web, domain
+ role :db,  domain, :primary => true
+
+ after "deploy:update", "deploy:layout"
+
+ namespace :deploy do
+
+    task :start do
+    end
+
+    task :stop do
+    end
+
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+    end
+
+    task :layout, :roles => :app do
+      run "mkdir -p #{File.join(current_path, 'data', 'collections')}"
+      run "mkdir -p #{File.join(current_path, 'data', 'schemas')}"
+    end
+ end
