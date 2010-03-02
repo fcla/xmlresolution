cf5891c542fdfb467574b3839f1a5326b3e3d7f8
Randy Fischer <rf@ufl.edu>
2010-03-02 17:13:32 -0500

    Taking proxy from RESOLVER_PROXY environment variable, set in apache config file: SetEnv RESOLVER_PROXY sake.fcla.edu.  Including XmlResolution now that everything is modularized.

--- app.rb
+++ app.rb
@@ -6,15 +6,21 @@ require 'xmlresolution.rb'
 # TODO: remove old collections on the fly (say, after a week...)
 # TODO: support HEAD, DELETE, etags and last-modified, accept headers
 
+include XmlResolution
+
 configure do
   $KCODE = 'UTF8'
-  XmlResolution::ResolverCollection.data_path = File.join(File.dirname(__FILE__), 'data')
-  XmlResolution::Logger.filename = File.join(File.dirname(__FILE__), 'logs', 'xmlresolution.log')
-  use Rack::CommonLogger, XmlResolution::Logger.new
-  set :proxy, 'sake.fcla.edu'   # TODO: better here - take it out of environment set by web server? Might make the most sense
+  ResolverCollection.data_path = File.join(File.dirname(__FILE__), 'data')
+  Logger.filename = File.join(File.dirname(__FILE__), 'logs', 'xmlresolution.log')
+  use Rack::CommonLogger, Logger.new
 end
 
 helpers do
+
+  def proxy
+    @env['RESOLVER_PROXY']
+  end
+
   def service_name
     'http://' + @env['SERVER_NAME'] + (@env['SERVER_PORT'] == '80' ? '' : ":#{@env['SERVER_PORT']}")
   end
@@ -59,13 +65,13 @@ end
 # The top level page gives an introduction to using this service.
 
 get '/' do
-  erb :site, :locals => { :base_url => service_name, :revision => XmlResolution::REVISION }
+  erb :site, :locals => { :base_url => service_name, :revision => REVISION }
 end
 
 # List all of the collections we've got
 
 get '/ieids/' do
-  erb :ieids, :locals => { :collections => XmlResolution::ResolverCollection.collections.sort }
+  erb :ieids, :locals => { :collections => ResolverCollection.collections.sort }
 end
 
 # Return a tarfile of all of the schemas we've collected for the xml documents submitted
@@ -74,18 +80,17 @@ end
 get '/ieids/:collection_id/' do |collection_id|
 
   content_type "text/plain"
-  [ halt 404, "No such collection #{collection_id}\n" ] unless XmlResolution::ResolverCollection.collection_exists? collection_id 
-
-  xrez = XmlResolution::ResolverCollection.new(collection_id)
-  last_modified xrez.httpdate   # bails immediately w/o tar creation overhead
+  [ halt 404, "No such collection #{collection_id}\n" ] unless ResolverCollection.collection_exists? collection_id 
 
   begin
+    xrez = ResolverCollection.new(collection_id)
+    last_modified xrez.httpdate   # bails immediately w/o tar creation overhead, if possible
     data = tar_up xrez
-  rescue XmlResolution::Http400Error => e
+  rescue Http400Error => e
     halt [ 400, e.message + "\n" ]
   rescue => e
-    XmlResolution::Logger.err env, e.message
-    e.backtrace.each { |line| XmlResolution::Logger.err env, line }
+    Logger.err env, e.message
+    e.backtrace.each { |line| Logger.err env, line }
     halt [ 500, "Error creating tarfile for collection #{collection_id}\n" ]
   else
     content_type "application/x-tar"
@@ -94,36 +99,38 @@ get '/ieids/:collection_id/' do |collection_id|
   end
 end
 
-# Create a new collection.
+# PUT a new collection resource.
 
 put '/ieids/:collection_id' do |collection_id|
   begin
     content_type 'text/plain'
-    if XmlResolution::ResolverCollection.collection_exists? collection_id
+    if ResolverCollection.collection_exists? collection_id
       status 200
-      "Collection #{collection_id} existed.\n"
+      "Collection #{collection_id} exists.\n"
     else
-      XmlResolution::ResolverCollection.new collection_id
+      ResolverCollection.new collection_id
       status 201
       "Collection #{collection_id} created.\n"
     end
-  rescue XmlResolution::Http400Error => e
+  rescue Http400Error => e
     halt [ 400, e.message + "\n" ]
   rescue => e
-    XmlResolution::Logger.err env, e.message
-    e.backtrace.each { |line| XmlResolution::Logger.err env, line }
+    Logger.err env, e.message
+    e.backtrace.each { |line| Logger.err env, line }
     halt [ 500, "We're sorry, there was a problem creating the collection #{collection_id}. Please contact customer support.\n" ]
   end
 end
 
-
 # POST an xmlfile to a resource collection.
 #
-# We expect Content-Type of enctype=multipart/form-data, which is used in your basic file upload form.
-# It expects behavior produced as the form input having type="file" name="xmlfile".  Additionally, we
-# require that the content disposition must supply an original filename.
-
-# TODO: I notice that a brand new collection might be created on the fly here: bug? Or feature?
+# We expect Content-Type of enctype=multipart/form-data, which is used
+# in your basic file upload form.  It expects behavior produced as the
+# form input having type="file" name="xmlfile".  Additionally, we
+# require that the content disposition must supply an original
+# filename.
+#
+# TODO: I notice that a brand new collection might be created on the
+# fly here: bug? Or feature?
 
 post '/ieids/:collection_id/' do |collection_id|
   begin
@@ -133,16 +140,16 @@ post '/ieids/:collection_id/' do |collection_id|
     halt [ 400, "Missing form data filename='...'\n" ]    unless filename = params['xmlfile'][:filename]
     halt [ 500, "Data unavailable (missing tempfile)\n" ] unless tempfile = params['xmlfile'][:tempfile]
 
-    xrez = XmlResolution::XmlResolver.new(tempfile.open.read, options.proxy)
+    xrez = XmlResolver.new(tempfile.open.read, proxy)
     xrez.filename = filename
 
-    XmlResolution::ResolverCollection.new(collection_id).add xrez
+    ResolverCollection.new(collection_id).add xrez
 
-  rescue XmlResolution::Http400Error => e
+  rescue Http400Error => e
     halt [ 400, e.message + "\n" ]
   rescue => e
-    XmlResolution::Logger.err env, e.message
-    e.backtrace.each { |line| XmlResolution::Logger.err env, line }
+    Logger.err env, e.message
+    e.backtrace.each { |line| Logger.err env, line }
     
     halt [ 500, "Can't process file #{filename} for collection #{collection_id}.\n"]
   else
@@ -153,7 +160,7 @@ post '/ieids/:collection_id/' do |collection_id|
 end
 
 get '/test/' do
-  erb :test, :locals => { :collections => XmlResolution::ResolverCollection.collections.sort }
+  erb :test, :locals => { :collections => ResolverCollection.collections.sort }
 end
 
 get '/test-form/:collection_id/' do |collection_id|
