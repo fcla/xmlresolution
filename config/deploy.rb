4ad7e54566b2d9b1747b6539eedaac2c19714f05
Randy Fischer <rf@ufl.edu>
2010-05-27 00:41:45 -0400

    testing what the effect this has when borken.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,27 @@
  set :application,       "xmlresolution"
- set :domain,            "xmlresolution.ripple.sacred.net"
  set :repository,        "http://github.com/daitss/xmlresolution.git"
  set :use_sudo,          false
  set :deploy_to,         "/opt/web-services/sites/#{application}"
  set :scm,               "git"
+ set :domain,            "xmlresolution.ripple.sacred.net"
  set :user,              "xmlrez"
-
 
  role :app, domain
  role :web, domain
  role :db,  domain, :primary => true
 
-# After we've successfully updated, we run these tasks: layout sets up
-# the directory structure and runs bundle to install our own gem
-# dependencies; rdoc builds library doco; restart touches the file
-# that instructs passenger phusion to restart the app.
+ # After we've successfully updated, we run these tasks: layout sets up
+ # the directory structure and runs bundle to install our own gem
+ # dependencies; rdoc builds library doco; restart touches the file
+ # that instructs passenger phusion to restart the app.
 
 after "deploy:update", "deploy:layout", "deploy:rdoc", "deploy:restart"
 
  namespace :deploy do
    
-   task :start do  # setup sudo command to restart apache?  doesn't seem necessary yet.
+borken
+
+   task :start do  # setup sudo command to restart apache?  Isn't necessary yet.
    end
    
    task :stop do
@@ -37,15 +38,16 @@ after "deploy:update", "deploy:layout", "deploy:rdoc", "deploy:restart"
        run "mkdir -p #{pathname}"       
        run "chmod -R ug+rwX #{pathname}" 
      end
+
+     run "cd #{current_path}; bundle install #{File.join(shared_path, "vendor")}"   # extract gems if necessary
      
-     ['collections', 'schemas'].each do |dir|       # want to preserve existing data, so let's keep these in the shared directory and link into them.
+     ['vendor', 'collections', 'schemas'].each do |dir|    # want to preserve existing data, so let's keep these in the shared directory and link into them.
        realname = File.join(shared_path, dir)
        linkname = File.join(current_path, 'data')
        run "ln -s #{realname} #{linkname}"
        run "chmod -R ug+rwX #{realname}" 
      end
 
-     run "cd #{current_path}; bundle install #{File.join(shared_path, "vendor")}"
    end
    
    task :rdoc, :roles => :app do                     # generate fresh rdoc.
