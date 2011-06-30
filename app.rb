1104a2fe0c63922a6307f78229931f673b8eb663
Randy Fischer <fischer@sacred.net>
2011-06-30 19:12:44 -0400

    do away with environment variables, instead Yet Another Config File

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'xmlresolution'
 include XmlResolution
 
 configure do
+  config = ResolverUtils.read_config(File.join(File.dirname(__FILE__), 'config.yml'))
+
   $KCODE = 'UTF8'         # Required for XML processing libraries.
 
   disable :logging        # Stop CommonLogger from logging to STDERR, please.
@@ -14,17 +16,21 @@ configure do
 
   set :raise_errors, false                  # We'll handle our own errors, thanks
 
-  set :proxy,       ENV['RESOLVER_PROXY']   # Where to find the tape robot (see SiloTape and TsmExecutor).
-  set :data_path,   ENV['DATA_ROOT']        # The collections and schema data live here.
+  set :proxy,       config.resolver_proxy   # Where to find the tape robot (see SiloTape and TsmExecutor).
+  set :data_path,   config.data_root        # The collections and schema data live here.
 
-  Logger.setup('XmlResolution')  # TODO: add vhost second arg
+  Logger.setup('XmlResolution', config.virtual_hostname)  # TODO: add vhost second arg
 
-  ENV['LOG_FACILITY'].nil? ? Logger.stderr : Logger.facility  = ENV['LOG_FACILITY']
+  if config.log_syslog_facility
+    Logger.facility = config.log_syslog_facility
+  else
+    Logger.stderr
+  end
 
-  use Rack::CommonLogger, Logger.new  # Bend CommonLogger to our will...
+  use Rack::CommonLogger, Logger.new(:info, 'Rack:')  # Bend CommonLogger to our will...
 
-  Logger.info "Starting #{XmlResolution.version.rev}."
-  Logger.info "Initializing with data directory #{ENV['DATA_ROOT']}; caching proxy is #{ENV['RESOLVER_PROXY'] || 'off' }."
+  Logger.info "Starting #{XmlResolution.version.rev}"
+  Logger.info "Initializing with data directory #{config.data_root}; caching proxy is #{config.virtual_hostname || 'off' }"
 end
 
 
@@ -37,4 +43,3 @@ begin
 rescue ScriptError => e
   Logger.err "Initialization Error: #{e.message}"
 end
-
--- config.ru
+++ config.ru
@@ -4,42 +4,6 @@ require 'bundler/setup'
 
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 
-# We use environment variables here to support two different service
-# scenarios. Note that only the top level app.rb pays attention to
-# these environment variables, and then only in a configure section.
-#
-# Apache is typically set up along the following lines, where the
-# virtual host configuration environment variables will override the
-# defaults in this file:
-#
-# <VirtualHost *:80>
-#   KeppAlive Off
-#   ServerName xmlresolution.example.com
-#   DocumentRoot ".../xmlresolution/public"
-#   SetEnv RESOLVER_PROXY sake.fcla.edu:3128
-#   SetEnv LOG_FACILITY LOG_LOCAL2
-#   SetEnv DATA_ROOT "/var/xmlresolution"
-#   <Directory ".../xmlresolution/public">
-#     Order allow,deny
-#     Allow from all
-#   </Directory>
-# </VirtualHost>
-#
-# We'd also like to be able to run using rackup, where the following
-# defaults will kick in:
-
-require 'socket'
-
-ENV['LOG_FACILITY']   ||= nil  # default to stderr; may be apache error log or console depending on how started
-ENV['DATA_ROOT']      ||= File.expand_path(File.join(File.dirname(__FILE__), 'data'))
-ENV['RESOLVER_PROXY'] ||= case Socket.gethostname
-                          when /sacred.net/          ; 'satyagraha.sacred.net:3128'
-                          when /fcla.edu/, /local/   ; 'sake.fcla.edu:3128'
-                          else
-                            STDERR.puts "No proxy assigned"
-                            nil
-                          end
-
 require 'sinatra'
 require 'app'
 
