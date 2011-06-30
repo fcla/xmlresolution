65ddea186995c3f58090f56267cce853b4a98f45
Randy Fischer <fischer@sacred.net>
2011-06-30 19:13:35 -0400

    bring this in line with changes made to store-master - must break this out into a common project

--- lib/xmlresolution/logger.rb
+++ lib/xmlresolution/logger.rb
@@ -52,37 +52,40 @@ require 'log4r/outputter/rollingfileoutputter'
 #  use Rack::CommonLogger, Logger.new
 
 class Logger
-  
+
   # This class variable lets us know if is safe to proceed; a Log4r
   # logging object has been set up and registered.  It must be set by
-  # a call to Logger.initialize_maybe - which one of the class
-  # initializers - Logger.filename=, Logger.facility= or Logger.stderr - 
-  # will take care of.
-  
-  @@log_name     = nil
-  @@process_name = nil
+  # a call to Logger.setup, followed by one or more of the log output
+  # initializers, namely Logger.filename=, Logger.facility= or
+  # Logger.stderr.
 
-  # Logger.setup process_name, service_name
+  @@virtual_hostname = nil
+  @@service_name     = nil
+
+  # Logger.setup service_name, virtual_hostname
   #
-  # Intialize the logging system.  Until called, all logging is 
+  # Intialize the logging system.  Until called, all logging is
   # a no-op.
   #
   # In logging the arguments are used as follows:
   #
-  # Dec 15 09:32:21 romeo-foxtrot PROCESS_NAME[45933]:  INFO SERVICE_NAME...
+  # Dec 15 12:38:26 romeo-foxtrot SiloPool[51470]:  INFO silos.test.fcla.edu
+  #                               --------               -------------------
+  #                               @@service_name         @@virtual_hostname
   #
-  # It is recommented the virtual host name be used for service_name
-  # and the general type service for process_name (e.g. XmlResolution,
-  # SiloPool, StoreMaster), so you might see a log entry like:
-  #
-  # Dec 15 12:38:26 romeo-foxtrot SiloPool[51470]:  INFO silos.test.fcla.edu: 
+  # It is recommented the virtual host name of the service be used for
+  # @@virtual_hostname but it will default to the hostname of the
+  # computer; @@service_name should not have any spaces, so the logs
+  # can be conveniently parsed.
 
-  def Logger.setup(process_name, service_name = Socket.gethostname)
-    @@log_name     = service_name
-    @@process_name = process_name
-    Log4r::Logger.new @@log_name
+
+  def Logger.setup(service_name, virtual_hostname = Socket.gethostname)
+    @@virtual_hostname = virtual_hostname
+    @@service_name     = service_name
+
+    Log4r::Logger.new @@virtual_hostname
   end
-      
+
   # Logger.filename = FILEPATH
   #
   # Intialize the logging system to write to the file named by
@@ -91,23 +94,23 @@ class Logger
   # We envision a number of ways to initialize this class before
   # use. We can set up for logging to files, syslog, stderr, or any
   # combination. At least one of them must be done before any actual
-  # logging occurs.  
+  # logging occurs.
   
   def Logger.filename= filepath
-    return unless (@@log_name and @@process_name)
-    Log4r::Logger[@@log_name].add Log4r::FileOutputter.new(@@process_name, { :filename => filepath, :trunc => false })
+    return unless (@@virtual_hostname and @@service_name)
+    Log4r::Logger[@@virtual_hostname].add Log4r::FileOutputter.new(@@service_name, { :filename => filepath, :trunc => false })
     filepath
   end
-  
+
   # Logger.stderr
   #
   # Initialize the logging system to write to STDERR.
-  
+
   def Logger.stderr
-    return unless (@@log_name and @@process_name)
-    Log4r::Logger[@@log_name].add Log4r::StderrOutputter.new(@@process_name)
+    return unless (@@virtual_hostname and @@service_name)
+    Log4r::Logger[@@virtual_hostname].add Log4r::StderrOutputter.new(@@service_name)
   end
-  
+
   # Logger.facility = FACILITY
   #
   # Intialize the logging system to write to syslog, using the
@@ -117,99 +120,113 @@ class Logger
   #    :LOG_LOCAL0
   #    :LOG_LOCAL1
   #       ...
-  #    :LOG_LOCAL7
+  #    :LOG_LOCAL9
   #
   # A typical syslog.conf entry using :LOG_LOCAL2 might look like this:
   #
   #    local2.error   /var/log/xmlresolution.error.log
   #    local2.warn    /var/log/xmlresolution.warn.log
   #    local2.info    /var/log/xmlresolution.info.log
-  # 
+  #
   # In the case of Mac OSX Snow Leopard, the above syslog.conf entry
   # means that a Logger.error(message) will get logged to
   # xmlresolution.error.log, xmlresolution.warn.log, and
   # xmlresolution.info.log.  Logger.warn(message) gets logged to
-  # xmlresolution.warn.log and xmlresolution.info.log. 
+  # xmlresolution.warn.log and xmlresolution.info.log.
   # Logger.info(message) will go to xmlresolution.info.log.  Other
-  # syslog deamons on other systems and alternative configurations 
+  # syslog deamons on other systems and alternative configurations
   # behave significantly differently.  But that's not your problem.
-  
+
   def Logger.facility= facility
-    return unless (@@log_name and @@process_name)
-    Log4r::Logger[@@log_name].add Log4r::SyslogOutputter.new(@@process_name, 'facility' => eval("Syslog::#{facility.to_s.upcase}"))
+    return unless (@@virtual_hostname and @@service_name)
+    facility = 'LOG_' + facility unless facility =~ /^LOG_/
+    Log4r::Logger[@@virtual_hostname].add Log4r::SyslogOutputter.new(@@service_name, 'facility' => eval("Syslog::#{facility.to_s.upcase}"))
     facility
   end
-  
+
   # Logger.err MESSAGE, [ ENV ]
   #
   # Log an error message MESSAGE, a string; The hash
   # ENV is typically the Sinatra @env object, but any hash with the
   # common PEP 333 keys could be used.
-  
+
   def Logger.err message, env = {}
-    return unless (@@log_name and @@process_name)
-    Log4r::Logger[@@log_name].error prefix(env) + message.chomp
+    return unless (@@virtual_hostname and @@service_name)
+    Log4r::Logger[@@virtual_hostname].error prefix(env) + message.chomp
   end
-  
+
   # Logger.warn MESSAGE, [ ENV ]
   #
   # Log a warning message MESSAGE, a string; The hash
   # ENV is typically the Sinatra @env object, but any hash with the
   # common PEP 333 keys could be used.
-  
+
   def Logger.warn message, env = {}
-    return unless (@@log_name and @@process_name)
-    Log4r::Logger[@@log_name].warn  prefix(env) + message.chomp
+    return unless (@@virtual_hostname and @@service_name)
+    Log4r::Logger[@@virtual_hostname].warn  prefix(env) + message.chomp
   end
-  
+
   # Logger.info MESSAGE, [ ENV ]
   #
   # Log an informative message MESSAGE, a string; The hash
   # ENV is typically the Sinatra @env object, but any hash with the
   # common PEP 333 keys could be used.
-  
+
   def Logger.info message, env = {}
-    return unless (@@log_name and @@process_name)
-    Log4r::Logger[@@log_name].info  prefix(env) + message.chomp
+    return unless (@@virtual_hostname and @@service_name)
+    Log4r::Logger[@@virtual_hostname].info  prefix(env) + message.chomp
   end
-  
-  # While we normally use the class methods to write our own log entries, we
-  # also have an object we can instantiate for Rack::CommonLogger to 
-  # use. For example:
+
+  # While we normally use the class methods to write our own log
+  # entries, we also have an object we can instantiate for
+  # Rack::CommonLogger or DataMapper::Logger to use. For example:
   #
   #  Logger.setup('XmlResolutionService', 'xrez.example.com')
   #  Logger.filename = 'my.log'
-  #  use Rack:CommonLogger, Logger.new
+  #  use Rack:CommonLogger, Logger.new(:info, 'Rack:')
+  #  DataMapper::Logger(Logger.new(:info, 'DataMapper:'), :debug)
 
-  def initialize
-    raise "The logging system has not been setup: use Logger.setup(service, hostname)." if (@@log_name.nil? or @@process_name.nil?)
-  end  
-  
+  @level = nil
+  @tag   = nil
+
+  def initialize level = :info, tag = ''
+    raise "Bad argument: if specified, the first argument must be one of :info, :warn, :error, but was :#{level}" unless [:info, :warn, :error].include? level
+    raise "Bad argument: if specified, the second argument must be a simple string, but was a #{tag.class}" unless tag.class == String
+    raise "The logging system has not been setup: use Logger.setup(service, hostname)." if (@@virtual_hostname.nil? or @@service_name.nil?)
+    @level = level
+    @tag   = tag.length > 0 ? tag : nil
+  end
+
   # log.write MESSAGE
   #
   # Rack::CommonLogger can be told to use as a logger any object that has a write method on
   # it. See the Logger#new method for an example of its use.
-  
+
   def write message
-    Log4r::Logger[@@log_name].info message.chomp
+    Log4r::Logger[@@virtual_hostname].send(@level, @tag ? "#{@tag} #{message.chomp}" : message.chomp)
   end
-  
+
   private
-  
+
   # prefix ENV
   #
   # Create an apache-style "METHOD /uri"  string, if possible, from the environment ENV.
-  # ENV is expected to be @env from a rack application,
+  # ENV is expected to be @env from a rack application
   #
   # Note that the object method #write will not use this.
-  
-  def Logger.prefix env    
-    return '' if env['REQUEST_METHOD'].nil? and env['PATH_INFO'].nil?
-    sprintf('"%s %s%s" ',
-            env['REQUEST_METHOD'],
-            env['PATH_INFO'],
-            (env['QUERY_STRING'].nil? or env['QUERY_STRING'].empty?) ? '' : '?' + env['QUERY_STRING'])
+
+  def Logger.prefix env
+    return '' if env['REQUEST_METHOD'].nil? or env['PATH_INFO'].nil?
+
+    sprintf('%s %s %s %s "%s%s"',
+            env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-",
+            env["REMOTE_USER"] || "-",
+            env["SERVER_PROTOCOL"],
+            env["REQUEST_METHOD"],
+            env["PATH_INFO"],
+            env["QUERY_STRING"].empty? ? "" : "?" + env["QUERY_STRING"]
+            )
   end
 
-  
+
 end # of class
